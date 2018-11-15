13ae4c8bae7a04969bf9233690f02df66b2431aa
Admin <admin@Qwertys-iMac.local>
2018-11-14 19:13:51 -0800

    added style to my search page

--- search.php
+++ search.php
@@ -1,5 +1,84 @@
 <?php
 
-echo $_GET["term"];
+	if(isset($_GET["term"])) {
+		$term = $_GET["term"];
+	}
+	else {
+		exit("You must enter a search term");
+	}
 
+	$type = isset($_GET["type"]) ? $_GET["type"] : "sites";
+
+
+	
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Doodle</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+</head>
+<body>
+
+	<div class="wrapper">
+	
+		<div class="header">
+
+
+			<div class="headerContent">
+
+				<div class="logoContainer">
+					<a href="index.php">
+						<img src="assets/images/doodleLogo.png">
+					</a>
+				</div>
+
+				<div class="searchContainer">
+
+					<form action="search.php" method="GET">
+
+						<div class="searchBarContainer">
+
+							<input class="searchBox" type="text" name="term">
+							<button class="searchButton">
+								<img src="assets/images/icons/search.png">
+							</button>
+						</div>
+
+					</form>
+
+				</div>
+
+			</div>
+
+
+			<div class="tabsContainer">
+
+				<ul class="tabList">
+
+					<li class="<?php echo $type == 'sites' ? 'active' : '' ?>">
+						<a href='<?php echo "search.php?term=$term&type=sites"; ?>'>
+							Sites
+						</a>
+					</li>
+
+					<li class="<?php echo $type == 'images' ? 'active' : '' ?>">
+						<a href='<?php echo "search.php?term=$term&type=images"; ?>'>
+							Images
+						</a>
+					</li>
+
+				</ul>
+
+
+			</div>
+
+
+
+		</div>
+	</div>
+
+</body>
+</html>
